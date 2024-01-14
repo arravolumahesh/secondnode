aa992ad79202e5cab348b6baebeeed0a48251d16
arravolumahesh <arravolumahesh@gamail.com>
2024-01-14 12:00:36 +0530

    Initial commit

--- main.js
+++ main.js
@@ -1,145 +1,273 @@
-const express=require("express");
-const mysql=require("mysql");
-const app=express()
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
 
-const bodyParser = require('body-parser');
+const app = express();
+
+// Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-app.use(express.json());
+// Create a MySQL connection pool
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "root123",
+  database: "mysqldb",
+});
 
+// Creating table if not exists
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error getting database connection: ", err);
+  } else {
+    const sql =
+      "CREATE TABLE IF NOT EXISTS mytable (id int, name varchar(20), subject varchar(20), marks int)";
+    connection.query(sql, (queryErr, result) => {
+      connection.release();
 
-const con=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root123",
-    database:"mysqldb",
-})
-
-// con.connect((err)=>{
-//     if (err){
-//         console.log(err)
-//     }else{
-//         console.log("Connected")
-//     }
-// })
-
-con.connect((err)=>{
-        if (err){
-            console.log(err)
-        }else{
-           var sql="CREATE TABLE IF NOT EXISTS mytable (id int,name varchar(20),subject varchar(20),marks int)";
-           con.query(sql,(err,result)=>{
-            if (err){
-                console.log(err)
-            }else{
-                console.log("Table Created")
-            }
-           })
-        }
-    })
-
-app.get("/fetch",(req,res)=>{
-    con.query("select * from mytable",function(err,result,fields){
-        if (err){
-            console.log(err)
-        }else{
-            res.send(result)
-        }
-    })
-})
-
-app.get("/fetchbyid/:id",(req,res)=>{
-    const fetched=req.params.id;
-    con.query("select * from mytable where id=?",fetched,(err,result)=>{
-        if (err){
-            console.log(err)
-        }else{
-           res.send(result)
-        }
-    })
-})
-
-app.post("/fetchpost",(req,res)=>{
-    const id=req.body.id;
-    const name=req.body.name;
-    const subject=req.body.subject;
-    const marks=req.body.marks;
-    
-    con.query("INSERT INTO mytable values(?,?,?,?)",[id,name,subject,marks],(err,result)=>{
-        if (err){
-            console.log(err)
-        }else{
-           res.send("Posted")
-        }
-    })
-})
-
-app.post("/postlist",(req,res)=>{
-
-    req.body.forEach(element => {
-        const id=element.id;
-        const name=element.name;
-        const subject=element.subject;
-        const marks=element.marks;
-        
-        con.query("INSERT INTO mytable values(?,?,?,?)",[id,name,subject,marks],(err,result)=>{
-            if (err){
-                return res.send(err)
-            }
-        })
+      if (queryErr) {
+        console.error("Error executing query: ", queryErr);
+      } else {
+        console.log("Table Created");
+      }
     });
+  }
+});
 
-    return res.send("List Posted")
-})
-
-
-
-app.put("/update/:id",(req,res)=>{
-    const upid=req.params.id;
-    const name=req.body.name;
-    const subject=req.body.subject;
-    const marks=req.body.marks;
-
-    con.query("UPDATE mytable SET name=?,subject=?,marks=? WHERE id=?",[upid,name,subject,marks],(err,result)=>{
-        if (err){
-            console.log(err)
-        }else{
-            res.send("Updated")
-        }
-    })
-})
-
-app.delete("/deletedata",(req,res)=>{
-    const delid=req.body.id;
-    con.query("DELETE FROM mytable",delid,(err,result)=>{
-        if (err){
-            console.log(err)
-        }else{
-            if (result.affectedRows==0){
-                console.log("id not present")
-            }else{
-                res.send("Deleted")
-            }
-        }
-    })
-})
-
-app.get("/getmax",(req,res)=>{
-    con.query("select max(marks) as maxmark from mytable",(err,result)=>{
-        if (err){
-            console.log(err)
-        }else{
-            res.send(result)
-            let data=JSON.parse(JSON.stringify(result));
-            console.log(data[0].maxmark)
-        }
-    })
-})
-
-app.listen(4000,(err)=>{
-    if (err){
-        console.log(err)
-    }else{
-        console.log("Port on 4000")
+// Fetch all data
+app.get("/fetch", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting database connection: ", err);
+      res.status(500).send("Internal Server Error");
+      return;
     }
-})
+
+    connection.query("SELECT * FROM mytable", (queryErr, result) => {
+      connection.release();
+
+      if (queryErr) {
+        console.error("Error executing query: ", queryErr);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.send(result);
+    });
+  });
+});
+
+// Fetch data by ID
+app.get("/fetchbyid/:id", (req, res) => {
+  const fetched = req.params.id;
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting database connection: ", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    connection.query(
+      "SELECT * FROM mytable WHERE id = ?",
+      fetched,
+      (queryErr, result) => {
+        connection.release();
+
+        if (queryErr) {
+          console.error("Error executing query: ", queryErr);
+          res.status(500).send("Internal Server Error");
+          return;
+        }
+
+        res.send(result);
+      }
+    );
+  });
+});
+
+// Add data
+app.post("/fetchpost", (req, res) => {
+  const id = req.body.id;
+  const name = req.body.name;
+  const subject = req.body.subject;
+  const marks = req.body.marks;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting database connection: ", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    connection.query(
+      "INSERT INTO mytable VALUES (?, ?, ?, ?)",
+      [id, name, subject, marks],
+      (queryErr, result) => {
+        connection.release();
+
+        if (queryErr) {
+          console.error("Error executing query: ", queryErr);
+          res.status(500).send("Internal Server Error");
+          return;
+        }
+
+        res.send("Posted");
+      }
+    );
+  });
+});
+
+// Add list of data
+app.post("/postlist", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting database connection: ", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const values = req.body.map((element) => [
+      element.id,
+      element.name,
+      element.subject,
+      element.marks,
+    ]);
+
+    connection.query(
+      "INSERT INTO mytable VALUES ?",
+      [values],
+      (queryErr, result) => {
+        connection.release();
+
+        if (queryErr) {
+          console.error("Error executing query: ", queryErr);
+          res.status(500).send("Internal Server Error");
+          return;
+        }
+
+        res.send("List Posted");
+      }
+    );
+  });
+});
+
+// Update data by ID
+app.put("/update/:id", (req, res) => {
+  const upid = req.params.id;
+  const name = req.body.name;
+  const subject = req.body.subject;
+  const marks = req.body.marks;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting database connection: ", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    connection.query(
+      "UPDATE mytable SET name=?, subject=?, marks=? WHERE id=?",
+      [name, subject, marks, upid],
+      (queryErr, result) => {
+        connection.release();
+
+        if (queryErr) {
+          console.error("Error executing query: ", queryErr);
+          res.status(500).send("Internal Server Error");
+          return;
+        }
+
+        res.send("Updated");
+      }
+    );
+  });
+});
+
+// Delete all data
+app.delete("/deletedata", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting database connection: ", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    connection.query("DELETE FROM mytable", (queryErr, result) => {
+      connection.release();
+
+      if (queryErr) {
+        console.error("Error executing query: ", queryErr);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      if (result.affectedRows === 0) {
+        console.log("No rows present");
+      } else {
+        res.send("Deleted");
+      }
+    });
+  });
+});
+
+app.delete("/deletedata/:id", (req, res) => {
+    const delid=req.params.id;
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.error("Error getting database connection: ", err);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+  
+      connection.query("DELETE FROM mytable WHERE id=?",delid, (queryErr, result) => {
+        connection.release();
+  
+        if (queryErr) {
+          console.error("Error executing query: ", queryErr);
+          res.status(500).send("Internal Server Error");
+          return;
+        }
+  
+        if (result.affectedRows === 0) {
+          console.log("No rows present");
+        } else {
+          res.send("Deleted");
+        }
+      });
+    });
+  });
+  
+// Fetch max marks
+app.get("/getmax", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error getting database connection: ", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    connection.query("SELECT MAX(marks) AS maxmark FROM mytable", (queryErr, result) => {
+      connection.release();
+
+      if (queryErr) {
+        console.error("Error executing query: ", queryErr);
+        res.status(500).send("Internal Server Error");
+        return;
+      }
+
+      res.send(result);
+      let data = JSON.parse(JSON.stringify(result));
+      console.log(data[0].maxmark);
+    });
+  });
+});
+
+// Start the server
+const port = 4000;
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server listening on port ${port}`);
+  }
+});
